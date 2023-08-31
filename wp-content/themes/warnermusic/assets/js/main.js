36845e652a110b97f0083f65384a8c87f9bd5734
phamday997 <vanday.pham@officience.com>
2023-08-31 07:19:16 +0700

    fix overflow on mobile and enable autoplay in banner slider

--- wp-content/themes/warnermusic/assets/js/main.js
+++ wp-content/themes/warnermusic/assets/js/main.js
@@ -55,7 +55,7 @@ function bannerSlider() {
         arrows: false,
         dots: true,
         infinite: true,
-        autoplay: false,
+        autoplay: true,
         fade: true,
         cssEase: 'linear',
         speed: 600,
