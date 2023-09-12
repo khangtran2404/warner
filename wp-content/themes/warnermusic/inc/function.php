1ae7d1b4b8a082389d44f2a7c2d570b94fe61dae
tranhuynhbaokhang <baokhang.tranhuynh@officience.com>
2023-09-12 23:29:52 +0700

    fix issue in artist homepage

--- wp-content/themes/warnermusic/inc/function.php
+++ wp-content/themes/warnermusic/inc/function.php
@@ -3,3 +3,4 @@ require_once('function/acf/acf.php');
 require_once('function/custom_post_type/custom_post_type.php');
 require_once('function/menu/menu_custom.php');
 require_once('function/widget/widget.php');
+require_once('function/global/functions.php');
