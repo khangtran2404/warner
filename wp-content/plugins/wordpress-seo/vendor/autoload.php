725512e5e35efc2625f7facdcbe7b1e857b3c1ba
phamday997 <vanday.pham@officience.com>
2024-03-16 11:08:45 +0700

    Upgrade plugin Yoast SEO

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit55b556110e23323e9246f73688fcb562::getLoader();
+return ComposerAutoloaderInit72de8c0f30caeb2046104cf7abfa68fd::getLoader();
