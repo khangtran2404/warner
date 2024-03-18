be8ac7b1f3bfb67c08e676360ec30f7051acdb07
phamday997 <vanday.pham@officience.com>
2024-03-18 20:51:21 +0700

    change input on modal to input contact form 7

--- wp-content/themes/warnermusic/single-jobs.php
+++ wp-content/themes/warnermusic/single-jobs.php
@@ -15,23 +15,27 @@ get_header();
 while ( have_posts() ) :
 	the_post(); ?>
     <div class="single-post-template single-job-template padding-page">
+        <div class="hero-heading">
+            <div class="container">
+                <div class="heading-group-link">
+                    <?php
+                        if(get_field('details_job','option')['back_to_the_parent_page']) {
+                            echo '<div class="back-to-parent"><a href="' . esc_url(get_field('details_job','option')['back_to_the_parent_page']) . '">'. __('BACK').'</a></div>';
+                        }
+                    ?>
+                    <h1 class="primary-title"><?= get_the_title() ?></h1>
+                </div>
+                <div class="apply-job-section">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#applyJobModal">
+                        <?= __( "Apply" ) ?>
+                    </button>
+                </div>
+            </div>
+        </div>
         <div class="container">
-            <div class="heading-group-link">
-                <?php
-                    if(get_field('back_to_the_parent_page','option')) {
-                        echo '<div class="back-to-parent"><a href="' . esc_url(get_field('back_to_the_parent_page','option')) . '">'. __('BACK').'</a></div>';
-                    }
-                ?>
-                <h1 class="primary-title"><?= get_the_title() ?></h1>
-            </div>
-            <div class="apply-job-section">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#applyJobModal">
-                    <?= __( "Apply" ) ?>
-                </button>
-            </div>
             <div class="information-section">
-                <div class="type"><?= get_field( 'job_type' ) ?></div>
+                <div class="type"><?= get_field( 'job_type' )['label'] ?></div>
                 <div class="date-period"><?= calculatePeriodTime( get_the_date() ) ?></div>
                 <div class="location"><?= get_field( 'job_location' ) ?></div>
                 <div class="expire-date"><?= __("Expiry date: ");?><?= get_field( 'job_expire_date' ) ?></div>
@@ -42,33 +46,28 @@ while ( have_posts() ) :
         </div>
     </div>
     <!-- Modal -->
-    <div class="modal fade " id="applyJobModal" tabindex="-1" aria-labelledby="applyJobModalLabel" aria-hidden="true">
+    <div class="modal fade modal-apply-job" id="applyJobModal" tabindex="-1" aria-labelledby="applyJobModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-body">
-                    <h1 style="color: black !important"><?= get_the_title() ?></h1>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Fullname" aria-label="Fullname">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Email" aria-label="Email">
-                        <input type="text" class="form-control" placeholder="Phone Number" aria-label="Phone Number">
-                    </div>
-                    <div class="input-group mb-3">
-                        <label class="input-group-text" for="inputGroupFile01">CV</label>
-                        <input type="file" class="form-control" id="inputGroupFile01">
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="text" class="form-control" placeholder="Online profile and portfolio" aria-label="Online profile and portfolio">
-                    </div>
-                    <div class="input-group mb-3">
-                        <textarea class="form-control" placeholder="Message" aria-label="Message"></textarea>
-                    </div>
+                <div class="modal-header">
+                    <h1 class="primary-title sync-name-job-cf7"><?= get_the_title() ?></h1>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?= __('Cancel') ?></button>
-                    <button type="button" class="btn btn-primary"><?= __('Submit') ?></button>
-                </div>
+                <!-- details \wp-content\themes\warnermusic\inc\views\wpcf7-soure\apply-job -->
+                <?php
+                    // includes modal body + footer
+                    $applyForm = get_field('details_job','option')['apply_form_shortcode'];
+                    if ($applyForm){
+                        echo do_shortcode($applyForm);
+                    }
+                ?>
+                <script>
+                    jQuery(document).ready(function() {
+                        let titleJob = jQuery(".sync-name-job-cf7").html();
+                        let valueCf7 = jQuery("#apply-job-sync");
+                    
+                        valueCf7.val(titleJob);
+                    });
+                </script>
             </div>
         </div>
     </div>
